b2f2e621569ebd2f6ac9f7c0a778bcc2d5597034
Luka Stefanovic <luka.stefanovic.ee144@gmail.com>
2026-03-17 18:03:48 +0100

    Added RingBuffer header with function definitions, ringbuffer struct and BUFFER_SIZE defined.

---
+++ include/RingBuffer.h
@@ -0,0 +1,30 @@
+#ifndef RING_BUFFER_H
+#define RING_BUFFER_H
+
+#include <stdint.h> 
+#include <stdbool.h>
+
+#define BUFFER_SIZE 256
+
+typedef struct
+{
+    uint8_t buffer[BUFFER_SIZE];
+    volatile uint16_t head;
+    volatile uint16_t tail;
+} RingBuffer;
+
+/* Initialization */
+void RingBuffer_Init(RingBuffer *rb);
+
+/* Data Operations */
+int RingBuffer_Push(RingBuffer *rb, uint8_t data);
+int RingBuffer_Pop(RingBuffer *rb, uint8_t *data);
+
+/* Status Checks */
+bool RingBuffer_EmptyCheck(const RingBuffer *rb);
+bool RingBuffer_FullCheck(const RingBuffer *rb);
+
+/* Utility */
+uint16_t RingBuffer_CurrentCapacity(const RingBuffer *rb);
+
+#endif
